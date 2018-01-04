fbdab4d88547a243befffe2ac726fd68155187cd
kyle_lin <kyle_lin@htc.com>
2018-01-04 17:38:41 +0800

    bash_tool: remove unuseful functions
    Symptom: N/A
    Root Cause: N/A
    Solution: N/A
    Project: N/A
    Note: N/A

--- .bash_tool.sh
+++ .bash_tool.sh
@@ -21,8 +21,8 @@ mmcomm() {
     ln -s /usr/lib/distcc prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin
     ./check_build_target.sh $1
     . build/envsetup.sh
-    #mm -j `distcc -j`  PRODUCT-htc_$1-userdebug 2>&1 |tee  ~/tmp/mmlog 
-    mm -j `distcc -j`  PRODUCT-htc_$1-$2 2>&1 |tee  ~/tmp/mmlog 
+    #mm -j `distcc -j`  PRODUCT-htc_$1-userdebug 2>&1 |tee  ~/tmp/mmlog
+    mm -j `distcc -j`  PRODUCT-htc_$1-$2 2>&1 |tee  ~/tmp/mmlog
     rm -f *\.d
     rm -f prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin
     mv  prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin.real  prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin
@@ -30,39 +30,6 @@ mmcomm() {
     cat /tmp/date1
     ls -l out/target/product/$1/system.img
 }
-mqi(){
-    . build/envsetup.sh
-    . cardhu_setup.sh
-    make -j20
-    ls -l out/target/product/cardhu/system.img
-}
-
-mqk(){
-    . build/envsetup.sh
-    . kernel_setup.sh
-    krebuild
-    ls -l out/target/product/cardhu/obj/KERNEL/arch/arm/boot/zImage
-}
-mq(){
-    date > /tmp/date1
-    mv  prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin  prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin.real
-    ln -s /usr/lib/distcc prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin
-
-    rm -f *\.d
-    rm -f prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin
-    mv  prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin.real  prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin
-    date >> /tmp/date1
-    cat /tmp/date1
-    echo $1
-}
-fastbootFlashAll(){
-    adb reboot bootloader
-    sudo ~/toolchain/bin/fastboot flash system system.img
-    sudo ~/toolchain/bin/fastboot flash userdata userdata.img
-    sudo ~/toolchain/bin/fastboot flash ramdisk ramdisk.img
-    sudo ~/toolchain/bin/fastboot reboot 
-}
-
 logcat(){
     echo `pwd`
     if [ "$1" = "" ];
@@ -73,7 +40,7 @@ logcat(){
     fi
 
     echo "$logfile"
-    /home/kyle/toolchain/bin/adb logcat |tee "$logfile" 
+    /home/kyle/toolchain/bin/adb logcat |tee "$logfile"
 }
 
 nvflashcardhu(){
@@ -85,80 +52,14 @@ nvflashcmd(){
     cp ./out/host/linux-x86/bin/nvflash nvtmp
     cp ./out/target//product/$PRODUCT/* nvtmp
     cd nvtmp
-    sudo ./nvflash --bct flash.bct --setbct --odmdata 0x80080105 --configfile flash.cfg --create --bl bootloader.bin --go 
+    sudo ./nvflash --bct flash.bct --setbct --odmdata 0x80080105 --configfile flash.cfg --create --bl bootloader.bin --go
     cd `gettop`
-}
-
-syncprojectcode(){
-    echo "sync $HC"
-    cd $HC
-    git pull
-    cd -
-
-    echo "sync $GB"
-    cd $GB
-    git pull
-    cd -
-
-    echo "sync $KERNEL"
-    cd $KERNEL
-    git pull
-    cd -
-}
-
-connect_to_desktopX(){
-    ssh  -p 222 -X kyle@10.9.113.202
-}
-connect_to_nb_desk(){
-    #rdesktop -f  -K -r sound:local -r clipboard:CLIPBOARD -r disk:desktop=/home/kyle/rdesktop -u kyle_lin -d htctaipei kyle_lin_w7p.htctaipei.htc.com.tw &> /dev/null &
-    remmina -c ~/bootsh/1395114018713.remmina &> /dev/null &
-    #rdesktop -f -g 95%  -r sound:local -r clipboard:PRIMARYCLIPBOARD -r disk:desktop=/home/kyle/rdesktop -u kyle_lin -d htctaipei 10.116.229.210&
 }
 
 openvpn(){
 sudo openconnect --script /etc/vpnc/vpnc-script https://vpntw.htc.com
 }
-connect_to_desktop(){
-    ssh -p 222  kyle@10.9.113.202
-}
-connect_to_buildServer(){
-    ssh -X kyle@10.112.26.172 
-}
-connect_to_blueserver(){
-    ssh  -p 22 -X user@10.9.156.2
-}
 
-# move io for performance
-function lmdd() {
-echo "lmdd start"
-count=$1
-if [ -z "$count" ]; then
-    count=64
-fi
-for ((i=1; i<=$count; i=i*2))
-do
-    size=$((256*i))
-    echo "wr"
-    sleep 3
-    adb shell /data/bin/lmdd if=internal of=/data/datafile bs=4k count=${size} flush=1 sync=1
-    adb shell "echo 3 > /proc/sys/vm/drop_caches"
-    echo "rd"
-    sleep 3
-    adb shell /data/bin/lmdd if=/data/datafile of=internal bs=4k
-done
-adb shell rm /data/datafile
-echo "lmdd done"
-}
-# file system test
-function iozone() {
-echo "iozone start"
-size=$1
-if [ -z "$size" ]; then
-    size=64m
-fi
-adb shell /data/bin/iozone -a -o -s $size -f /data/datafile
-echo "iozone done"
-}
 # database insertion test
 function dbinsert() {
 echo "database insertion"
@@ -259,7 +160,7 @@ restart_pnpmgr(){
     sudo adb shell stop pnpmgr && sudo adb shell start pnpmgr
 }
 
-remove_watermark_sense4(){                                                                                                                                                                                                                     
+remove_watermark_sense4(){
 sudo adb remount
 sudo adb pull /data/dalvik-cache/system@framework@services.jar@classes.dex system@framework@services.jar@classes.dex
 sudo perl -pi -e 's/com\/android\/server\/ShowWatermarkService/cmm\/android\/server\/ShowWatermarkService/g' system@framework@services.jar@classes.dex
@@ -353,90 +254,117 @@ run_cfbenchmark(){
 }
 
 android_input_command_help(){
-    echo '0 -->  "KEYCODE_UNKNOWN" 
-    1 -->  "KEYCODE_MENU" 
-    2 -->  "KEYCODE_SOFT_RIGHT" 
-    3 -->  "KEYCODE_HOME" 
-    4 -->  "KEYCODE_BACK" 
-    5 -->  "KEYCODE_CALL" 
-    6 -->  "KEYCODE_ENDCALL" 
-    7 -->  "KEYCODE_0" 
-    8 -->  "KEYCODE_1" 
-    9 -->  "KEYCODE_2" 
-    10 -->  "KEYCODE_3" 
-    11 -->  "KEYCODE_4" 
-    12 -->  "KEYCODE_5" 
-    13 -->  "KEYCODE_6" 
-    14 -->  "KEYCODE_7" 
-    15 -->  "KEYCODE_8" 
-    16 -->  "KEYCODE_9" 
-    17 -->  "KEYCODE_STAR" 
-    18 -->  "KEYCODE_POUND" 
-    19 -->  "KEYCODE_DPAD_UP" 
-    20 -->  "KEYCODE_DPAD_DOWN" 
-    21 -->  "KEYCODE_DPAD_LEFT" 
-    22 -->  "KEYCODE_DPAD_RIGHT" 
-    23 -->  "KEYCODE_DPAD_CENTER" 
-    24 -->  "KEYCODE_VOLUME_UP" 
-    25 -->  "KEYCODE_VOLUME_DOWN" 
-    26 -->  "KEYCODE_POWER" 
-    27 -->  "KEYCODE_CAMERA" 
-    28 -->  "KEYCODE_CLEAR" 
-    29 -->  "KEYCODE_A" 
-    30 -->  "KEYCODE_B" 
-    31 -->  "KEYCODE_C" 
-    32 -->  "KEYCODE_D" 
-    33 -->  "KEYCODE_E" 
-    34 -->  "KEYCODE_F" 
-    35 -->  "KEYCODE_G" 
-    36 -->  "KEYCODE_H" 
-    37 -->  "KEYCODE_I" 
-    38 -->  "KEYCODE_J" 
-    39 -->  "KEYCODE_K" 
-    40 -->  "KEYCODE_L" 
-    41 -->  "KEYCODE_M" 
-    42 -->  "KEYCODE_N" 
-    43 -->  "KEYCODE_O" 
-    44 -->  "KEYCODE_P" 
-    45 -->  "KEYCODE_Q" 
-    46 -->  "KEYCODE_R" 
-    47 -->  "KEYCODE_S" 
-    48 -->  "KEYCODE_T" 
-    49 -->  "KEYCODE_U" 
-    50 -->  "KEYCODE_V" 
-    51 -->  "KEYCODE_W" 
-    52 -->  "KEYCODE_X" 
-    53 -->  "KEYCODE_Y" 
-    54 -->  "KEYCODE_Z" 
-    55 -->  "KEYCODE_COMMA" 
-    56 -->  "KEYCODE_PERIOD" 
-    57 -->  "KEYCODE_ALT_LEFT" 
-    58 -->  "KEYCODE_ALT_RIGHT" 
-    59 -->  "KEYCODE_SHIFT_LEFT" 
-    60 -->  "KEYCODE_SHIFT_RIGHT" 
-    61 -->  "KEYCODE_TAB" 
-    62 -->  "KEYCODE_SPACE" 
-    63 -->  "KEYCODE_SYM" 
-    64 -->  "KEYCODE_EXPLORER" 
-    65 -->  "KEYCODE_ENVELOPE" 
-    66 -->  "KEYCODE_ENTER" 
-    67 -->  "KEYCODE_DEL" 
-    68 -->  "KEYCODE_GRAVE" 
-    69 -->  "KEYCODE_MINUS" 
-    70 -->  "KEYCODE_EQUALS" 
-    71 -->  "KEYCODE_LEFT_BRACKET" 
-    72 -->  "KEYCODE_RIGHT_BRACKET" 
-    73 -->  "KEYCODE_BACKSLASH" 
-    74 -->  "KEYCODE_SEMICOLON" 
-    75 -->  "KEYCODE_APOSTROPHE" 
-    76 -->  "KEYCODE_SLASH" 
-    77 -->  "KEYCODE_AT" 
-    78 -->  "KEYCODE_NUM" 
-    79 -->  "KEYCODE_HEADSETHOOK" 
-    80 -->  "KEYCODE_FOCUS" 
-    81 -->  "KEYCODE_PLUS" 
-    82 -->  "KEYCODE_MENU" 
-    83 -->  "KEYCODE_NOTIFICATION" 
-    84 -->  "KEYCODE_SEARCH" 
+    echo '0 -->  "KEYCODE_UNKNOWN"
+    1 -->  "KEYCODE_MENU"
+    2 -->  "KEYCODE_SOFT_RIGHT"
+    3 -->  "KEYCODE_HOME"
+    4 -->  "KEYCODE_BACK"
+    5 -->  "KEYCODE_CALL"
+    6 -->  "KEYCODE_ENDCALL"
+    7 -->  "KEYCODE_0"
+    8 -->  "KEYCODE_1"
+    9 -->  "KEYCODE_2"
+    10 -->  "KEYCODE_3"
+    11 -->  "KEYCODE_4"
+    12 -->  "KEYCODE_5"
+    13 -->  "KEYCODE_6"
+    14 -->  "KEYCODE_7"
+    15 -->  "KEYCODE_8"
+    16 -->  "KEYCODE_9"
+    17 -->  "KEYCODE_STAR"
+    18 -->  "KEYCODE_POUND"
+    19 -->  "KEYCODE_DPAD_UP"
+    20 -->  "KEYCODE_DPAD_DOWN"
+    21 -->  "KEYCODE_DPAD_LEFT"
+    22 -->  "KEYCODE_DPAD_RIGHT"
+    23 -->  "KEYCODE_DPAD_CENTER"
+    24 -->  "KEYCODE_VOLUME_UP"
+    25 -->  "KEYCODE_VOLUME_DOWN"
+    26 -->  "KEYCODE_POWER"
+    27 -->  "KEYCODE_CAMERA"
+    28 -->  "KEYCODE_CLEAR"
+    29 -->  "KEYCODE_A"
+    30 -->  "KEYCODE_B"
+    31 -->  "KEYCODE_C"
+    32 -->  "KEYCODE_D"
+    33 -->  "KEYCODE_E"
+    34 -->  "KEYCODE_F"
+    35 -->  "KEYCODE_G"
+    36 -->  "KEYCODE_H"
+    37 -->  "KEYCODE_I"
+    38 -->  "KEYCODE_J"
+    39 -->  "KEYCODE_K"
+    40 -->  "KEYCODE_L"
+    41 -->  "KEYCODE_M"
+    42 -->  "KEYCODE_N"
+    43 -->  "KEYCODE_O"
+    44 -->  "KEYCODE_P"
+    45 -->  "KEYCODE_Q"
+    46 -->  "KEYCODE_R"
+    47 -->  "KEYCODE_S"
+    48 -->  "KEYCODE_T"
+    49 -->  "KEYCODE_U"
+    50 -->  "KEYCODE_V"
+    51 -->  "KEYCODE_W"
+    52 -->  "KEYCODE_X"
+    53 -->  "KEYCODE_Y"
+    54 -->  "KEYCODE_Z"
+    55 -->  "KEYCODE_COMMA"
+    56 -->  "KEYCODE_PERIOD"
+    57 -->  "KEYCODE_ALT_LEFT"
+    58 -->  "KEYCODE_ALT_RIGHT"
+    59 -->  "KEYCODE_SHIFT_LEFT"
+    60 -->  "KEYCODE_SHIFT_RIGHT"
+    61 -->  "KEYCODE_TAB"
+    62 -->  "KEYCODE_SPACE"
+    63 -->  "KEYCODE_SYM"
+    64 -->  "KEYCODE_EXPLORER"
+    65 -->  "KEYCODE_ENVELOPE"
+    66 -->  "KEYCODE_ENTER"
+    67 -->  "KEYCODE_DEL"
+    68 -->  "KEYCODE_GRAVE"
+    69 -->  "KEYCODE_MINUS"
+    70 -->  "KEYCODE_EQUALS"
+    71 -->  "KEYCODE_LEFT_BRACKET"
+    72 -->  "KEYCODE_RIGHT_BRACKET"
+    73 -->  "KEYCODE_BACKSLASH"
+    74 -->  "KEYCODE_SEMICOLON"
+    75 -->  "KEYCODE_APOSTROPHE"
+    76 -->  "KEYCODE_SLASH"
+    77 -->  "KEYCODE_AT"
+    78 -->  "KEYCODE_NUM"
+    79 -->  "KEYCODE_HEADSETHOOK"
+    80 -->  "KEYCODE_FOCUS"
+    81 -->  "KEYCODE_PLUS"
+    82 -->  "KEYCODE_MENU"
+    83 -->  "KEYCODE_NOTIFICATION"
+    84 -->  "KEYCODE_SEARCH"
     85 -->  "TAG_LAST_KEYCODE"                                                                                     '
+}
+
+server_farm(){
+ssh 10.113.128.4 -l 'htctaipei\kyle_lin'
+}
+skip_oobe(){
+    adb shell settings put global device_provisioned 1
+    adb shell settings put secure user_setup_complete 1
+    adb shell settings put system setup_wizard_has_run 1
+    adb shell pm disable com.google.android.setupwizard
+}
+enable_htc_logs(){
+    adb shell 'echo 1C187F1818181818181818181818181818181818181818181818181818181818 > /etc/hldm.bin'
+}
+
+setfps(){
+      adb shell "service call SurfaceFlinger 3007 i32 $1"
+}
+export errorpattern="FATAL EXCEPTION|WATCHDOG TIMEOUT|Exit zygote|OutOfMemoryError|App crashed| WATCHDOG KILLING|ANR in|CORE_DUMP_DETECT = 1|Camera Control: Test Activity not resume| failed to come online|ServiceManager: Waiting for service|VoldConnector"
+
+systrace_wake_up_issue(){
+    sed -i s#success=\(.d\+\)\ #success=\(\\\d\+\)\ \)\?#g $1
+}
+
+update_mytask()
+{
+    firefox --new-window  http://pqm.ssd.htc.com.tw:85/LogDailyTask.aspx && sleep 2 && xmacroplay -d 2000 "$DISPLAY" < ~/bin/auto_mytask2.src
 }
